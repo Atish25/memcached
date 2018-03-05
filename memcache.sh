de0347504b2e849cccac828ce4c3833941ef336b
Atish Das <das91.atish@gmail.com>
2018-03-05 16:48:02 +0530

    update ipv6
    update ipv6

--- memcache.sh
+++ memcache.sh
@@ -16,12 +16,15 @@ netstat -an | grep ":11211"
 else
 yum install net-tools -y && yum install curl -y;
 fi
+echo -e "\e[31;43m*****DISABLE IPV6 *****\e[0m"
+sysctl -w net.ipv6.conf.all.disable_ipv6=1
 #publicip=curl icanhazip.com
 publicip=$(curl icanhazip.com)
 if [ -e '/usr/bin/nmap' ]; then
 echo -e "\e[31;43m*****CHECK MEMCACHED PORT STATUS *****\e[0m" && nmap -p 11211 $publicip
  else
 yum install nmap -y && echo -e "\e[31;43m*****CHECK MEMCACHED PORT STATUS *****\e[0m" && nmap -p 11211 $publicip
+rm -rf memcache.sh
 exit 1
 fi
 rm -rf memcache.sh
